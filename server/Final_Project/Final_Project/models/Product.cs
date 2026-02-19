0886c114d39b0ed4d4fce88844622190f11e2b33
chayashiff <ch4141680@gmail.com>
2026-02-19 23:42:48 +0200

    add scaffold tables

---
+++ server/Final_Project/Final_Project/models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Final_Project.models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int? StockQuantity { get; set; }
+
+    public string? ImageUrl { get; set; }
+}
